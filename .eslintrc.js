01007b66480f1a3220ebb5da9dc69afd536f6d79
bacebu4 <vasua14735@icloud.com>
2021-06-12 07:36:17 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'max-classes-per-file': 0,
+    'no-console': 0,
+    'object-curly-newline': 0,
+  },
+};
